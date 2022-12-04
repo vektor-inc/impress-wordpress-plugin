3f9de9f242464fa2ec36b46f04a50938435ba178
kurudrive <kurudrive@gmail.com>
2022-12-05 03:53:29 +0900

    first commit

---
+++ impress-wordpress-textbook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Impress WordPress Textbook
+ * Plugin URI:      https://book.impress.co.jp/books/この書籍のURL
+ * Description:     いちばんやさしいWordPressの教本 第6版 素材同梱プラグイン
+ * Author:          Vektor,Inc.
+ * Author URI:
+ * Text Domain:     impress-wordpress-textbook
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         IMPRESS_WORDPRESS_TEXTBOOK
+ */
+
+include( dirname( __FILE__ ) . '/patterns-data/class-register-patterns-from-json.php' );
