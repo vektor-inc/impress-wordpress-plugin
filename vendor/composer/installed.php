a6dee42f6f36f59f419d08b063a56a25f16b1c81
kurudrive <kurudrive@gmail.com>
2022-12-05 04:17:54 +0900

    update checker

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vektor-inc/impress-wordpress-textbook',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3f9de9f242464fa2ec36b46f04a50938435ba178',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vektor-inc/impress-wordpress-textbook' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3f9de9f242464fa2ec36b46f04a50938435ba178',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v4.13',
+            'version' => '4.13.0.0',
+            'reference' => '6eb27a6911e0e0880d09e5b11f577d3f688f7da7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
